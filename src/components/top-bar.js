aee982cfc824965f7f6944cec866036fc3a557c8
tijlb99 <tijl.bormans@student.kuleuven.be>
2020-04-23 09:21:27 +0200

    search bar now works (but not completely)

--- src/components/top-bar.js
+++ src/components/top-bar.js
@@ -1,14 +1,28 @@
 import React from "react"
+import { StaticQuery, graphql } from "gatsby"
+import Search from "./search"
 
 export default () => {
   return (
-    <div className="topbar">
-      <a href="url">Module</a>
-      <a>|</a>
-      <a href="url">Class</a>
-      <a>|</a>
-      <a href="url">Use</a>
-      <input type="text" placeholder="search"/>  
-    </div>
+    <StaticQuery
+    query={graphql`
+      query SearchIndexQuery {
+        siteSearchIndex {
+          index
+        }
+      }
+    `}
+    render={data => (          
+      <div className="topbar">
+        <a href="url">Module</a>
+        <a>|</a>
+        <a href="url">Class</a>
+        <a>|</a>
+        <a href="url">Use</a>
+        {/* <input type="text" placeholder="search"/>   */}
+        <Search searchIndex={data.siteSearchIndex.index} />
+      </div>
+    )}
+  />
   )
 }
