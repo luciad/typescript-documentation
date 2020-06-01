722e0968ca078161d555ee661e4a47ccbc027879
tijlb99 <tijl.bormans@student.kuleuven.be>
2020-06-01 19:09:52 +0200

    added more util tests

--- src/__tests__/util/util.js
+++ src/__tests__/util/util.js
@@ -1,5 +1,5 @@
 
-import { fixModuleName, pathToModule, pathToExport, getComments }  from "../../util/util"
+import { fixModuleName, pathToModule, pathToExport, getComments, getParameters, getSignatures, getFlags }  from "../../util/util"
 
 test('fixModuleName 404', () => {
     expect(fixModuleName(null)).toBe("404")
@@ -67,4 +67,59 @@ test('getComments empty test', () => {
     expect(retVals.shortText).toBe("")
     expect(retVals.returns).toEqual([])
     expect(retVals.tags).toEqual([])
+})
+
+test('getParameters simple name type test', () => {
+    let data = {
+        parameters: [{
+            name: "param1",
+            type: {
+                name: "type1"
+        }},
+        {
+            name: "param2",
+            type: {
+                name: "type2"
+        }}]
+    }
+    let params = getParameters(data)
+    expect(params[0].name).toEqual("param1")
+    expect(params[0].type).toEqual("type1")
+    expect(params[1].name).toEqual("param2")
+    expect(params[1].type).toEqual("type2")
+})
+
+test('getSignatures simple test', () => {
+    let data = {
+        setSignature: [
+            "setSign1",
+            "setSign2"
+        ],
+        getSignature: [
+            "getSign"
+        ],
+        signatures: [
+            "sign"
+        ]
+    }
+
+    let signatures = getSignatures(data)
+    expect(signatures.length).toEqual(4)
+    let expected = ["setSign1", "setSign2", "getSign", "sign"]
+    for(let signature of expected){
+        expect(signatures.includes(signature)).toBeTruthy()
+    }
+})
+
+test('getFlags simple test', () => {
+    let data = {
+        flags: {
+            "isExported": true,
+            "flag1": true
+          },
+    }
+    let flags = getFlags(data)
+    expect(flags.includes("isExported")).toBeTruthy()
+    expect(flags.includes("flag1")).toBeTruthy()
+    expect(flags.length).toEqual(2)
 })
