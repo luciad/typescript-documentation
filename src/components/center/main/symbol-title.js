9e17b610c6c1a6a7603b7ce081ab35e6bdb8bc9d
tijlb99 <tijl.bormans@student.kuleuven.be>
2020-07-23 16:31:37 +0200

    isOptional marker bugfix

--- src/components/center/main/symbol-title.js
+++ src/components/center/main/symbol-title.js
@@ -5,21 +5,23 @@ import Type from "./general/body/type/type"
 export default ({ data, link }) => {
   return (
       <div className="symbol-title inline-block">
-        <div className="title">
-            {link ? <Link to={data.fields.path}>{data.name}</Link> : data.name}
-            {data.typeParameter &&
-                <>
-                    {"<"}
-                    {data.typeParameter.map((tp, i) =>
-                    <>
-                        {i > 0 && <>, </>}
-                        <Type data={{type: tp}}/>
-                    </>)}
-                    {">"}
-                </>}
-        </div>
-        {data.flags.isOptional &&
-            <div className="bottom optional-mark" title="isOptional">&nbsp;?&nbsp;</div>}
+        <div className="sidecontainer">
+          <div className="title">
+              {link ? <Link to={data.fields.path}>{data.name}</Link> : data.name}
+              {data.typeParameter &&
+                  <>
+                      {"<"}
+                      {data.typeParameter.map((tp, i) =>
+                      <>
+                          {i > 0 && <>, </>}
+                          <Type data={{type: tp}}/>
+                      </>)}
+                      {">"}
+                  </>}
+          </div>
+          {data.flags.isOptional &&
+              <div className="bottom optional-mark inline-block" title="isOptional">&nbsp;?&nbsp;</div>}
+      </div>
     </div>
   )
 }
