f5d43fc6220f873bb5b6299a831b6ba5e4149c1b
tijlb99 <tijl.bormans@student.kuleuven.be>
2020-08-16 10:44:30 +0200

    moved typeDefs to its own file

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,7 +4,8 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 const fs = require("fs")
- const util = require("./src/util/util");
+const util = require("./src/util/util");
+const typeDefs = require("./type-defs")
 const documentation = require("./content/docu.json");
 const documentationPath = require.resolve("./content/docu.json");
 const symbolTemplate = require.resolve("./src/components/center/main/template/symbol");
@@ -161,129 +162,7 @@ async function onCreateNode({
 
 exports.createSchemaCustomization = ({ actions }) => {
   const { createTypes } = actions
-  /**
-   * https://graphql.org/learn/schema/
-   * https://www.digitalocean.com/community/tutorials/graphql-graphql-sdl
-   */
-  const typeDefs = `
-    type Module implements Node {
-      name: String!
-      kindString: String!
-      children: [Symbol!]
-      comment: commentField
-    }
-
-    type Symbol implements Node {
-      name: String!
-      kindString: String!
-      id: ID!
-      children: [Symbol!]
-      comment: commentField
-      flags: flagsField
-      defaultValue: String
-      extendedTypes: [typeField!]
-      extendedBy: [typeField!]
-      implementedTypes: [typeField!]
-      implementedBy: [typeField!]
-      implementationOf: typeField
-      signatures: [signature!]
-      getSignature: [signature!]
-      setSignature: [signature!]
-      overwrites: typeField
-      inheritedFrom: typeField
-      type: typeField
-      typeParameter: [parametersField!]
-      target: Int
-    }
-
-    type signature {
-      name: String!
-      kindString: String!
-      comment: commentField
-      type: typeField
-      flags: flagsField
-      parameters: [parametersField]
-      overwrites: typeField
-      inheritedFrom: typeField
-      implementationOf: typeField
-      typeParameter: [parametersField!]
-    }
-
-    type commentField {
-      shortText: String
-      text: String
-      returns: String
-      tags: [tagField!]
-    }
-
-    type tagField {
-      tag: String!
-      text: String
-    }
-
-    type typeField {
-      checkType: typeField
-      comment: commentField
-      constraint: typeField
-      declaration: declarationField
-      elementType: typeField
-      elements: [typeField]
-      extendsType: typeField
-      falseType: typeField
-      id: Int
-      indexType: typeField
-      name: String
-      objectType: typeField
-      operator: String
-      queryType: typeField
-      target: typeField
-      targetType: typeField
-      trueType: typeField
-      type: String
-      typeArguments: [typeField]
-      types: [typeField!]
-      value: String
-    }
-
-    type declarationField {
-      id: Int
-      name: String
-      kindString: String
-      flags: flagsField
-      signatures: [signature!]
-      children: [parametersField!]
-      indexSignature: [signature!]
-    }
-
-
-    type parametersField {
-      id: Int
-      name: String
-      kindString: String
-      flags: flagsField
-      comment: commentField
-      type: typeField
-      tags: [tagField!]
-      defaultValue: String
-    }
-
-    type flagsField {
-      hasExportAssignment: Boolean
-      isAbstract: Boolean
-      isConst: Boolean
-      isConstructorProperty: Boolean
-      isExported: Boolean
-      isOptional: Boolean
-      isLet: Boolean
-      isPrivate: Boolean
-      isProtected: Boolean
-      isPublic: Boolean
-      isReadonly: Boolean
-      isRest: Boolean
-      isStatic: Boolean
-    }
-  `
-  createTypes(typeDefs)
+  createTypes(typeDefs.typeDefs)
 }
 
 exports.onCreateNode = onCreateNode;
---
+++ type-defs.js
@@ -0,0 +1,126 @@
+  /**
+   * https://graphql.org/learn/schema/
+   * https://www.digitalocean.com/community/tutorials/graphql-graphql-sdl
+   */
+const typeDefs = `
+    type Module implements Node {
+      name: String!
+      kindString: String!
+      children: [Symbol!]
+      comment: commentField
+    }
+
+    type Symbol implements Node {
+      name: String!
+      kindString: String!
+      id: ID!
+      children: [Symbol!]
+      comment: commentField
+      flags: flagsField
+      defaultValue: String
+      extendedTypes: [typeField!]
+      extendedBy: [typeField!]
+      implementedTypes: [typeField!]
+      implementedBy: [typeField!]
+      implementationOf: typeField
+      signatures: [signature!]
+      getSignature: [signature!]
+      setSignature: [signature!]
+      overwrites: typeField
+      inheritedFrom: typeField
+      type: typeField
+      typeParameter: [parametersField!]
+      target: Int
+    }
+
+    type signature {
+      name: String!
+      kindString: String!
+      comment: commentField
+      type: typeField
+      flags: flagsField
+      parameters: [parametersField]
+      overwrites: typeField
+      inheritedFrom: typeField
+      implementationOf: typeField
+      typeParameter: [parametersField!]
+    }
+
+    type commentField {
+      shortText: String
+      text: String
+      returns: String
+      tags: [tagField!]
+    }
+
+    type tagField {
+      tag: String!
+      text: String
+    }
+
+    type typeField {
+      checkType: typeField
+      comment: commentField
+      constraint: typeField
+      declaration: declarationField
+      elementType: typeField
+      elements: [typeField]
+      extendsType: typeField
+      falseType: typeField
+      id: Int
+      indexType: typeField
+      name: String
+      objectType: typeField
+      operator: String
+      queryType: typeField
+      target: typeField
+      targetType: typeField
+      trueType: typeField
+      type: String
+      typeArguments: [typeField]
+      types: [typeField!]
+      value: String
+    }
+
+    type declarationField {
+      id: Int
+      name: String
+      kindString: String
+      flags: flagsField
+      signatures: [signature!]
+      children: [parametersField!]
+      indexSignature: [signature!]
+    }
+
+
+    type parametersField {
+      id: Int
+      name: String
+      kindString: String
+      flags: flagsField
+      comment: commentField
+      type: typeField
+      tags: [tagField!]
+      defaultValue: String
+    }
+
+    type flagsField {
+      hasExportAssignment: Boolean
+      isAbstract: Boolean
+      isConst: Boolean
+      isConstructorProperty: Boolean
+      isExported: Boolean
+      isOptional: Boolean
+      isLet: Boolean
+      isPrivate: Boolean
+      isProtected: Boolean
+      isPublic: Boolean
+      isReadonly: Boolean
+      isRest: Boolean
+      isStatic: Boolean
+    }
+  `
+
+  module.exports = {
+    typeDefs
+  }
