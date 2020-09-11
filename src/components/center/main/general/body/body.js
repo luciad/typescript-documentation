7917b38b9d34e5740837203658b95f70835870ed
tijlb99 <tijl.bormans@student.kuleuven.be>
2020-09-11 20:36:57 +0200

    expand children now only shows shortText

--- src/components/center/main/general/body/body.js
+++ src/components/center/main/general/body/body.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import BodySummary from "./body-summary"
+import SearchLink from "../search-link"
+import Text from "../text"
 import Leaf from "../body/children/leaf"
 
 /**
@@ -19,12 +21,35 @@ export default ({ data, requireExpand }) => {
               <summary>
                 Expand children
               </summary>
-            <Leaves data={data}/>
+            <Expansion data={data}/>
             </details>
       : <Leaves data={data}/>}
     </div>
   );
 };
+
+class Expansion extends Component {
+  render() {
+    const data=this.props.data
+    if(!data.children) return null
+
+    return (
+      <table>
+        {data.children.map(child => (
+          <tr>
+            <td>
+              <SearchLink data={{text: child.name, id: child.id}}/>
+            </td>
+            <td>
+              {child.comment &&
+                <Text data={child.comment.shortText}/>}
+            </td>
+          </tr>
+        ))}
+      </table>
+    )
+  }
+}
 
 class Leaves extends Component {
   render() {
