01e5da5f4547a3fd8dc7fe1a899c16bd4c9787d9
tijlb99 <tijl.bormans@student.kuleuven.be>
2020-10-10 21:07:00 +0200

    removed children summary for exports as this is no longer used

--- src/components/body/children/children-summary.js
+++ src/components/body/children/children-summary.js
@@ -4,47 +4,36 @@ import SearchLink from "../general/search-link"
 
 /**
  * List of children
- * Makes them a link if they're included in data.exports
  *
  * Contains:
  * - List of all childrens icons and names
- *  - If it's in data.exports, it's a link to another page
- *      otherwise it' s a link to the leaf version of the child
+ *  - a link to the leaf version of the child
  *
  */
 export default ({ data }) => {
   if(!data) return null
   const children = data.children
   if(!children || children.length ===  0) return null
-  let exportIds = data.exports ? data.exports.map(exp => exp.id) : []
 
   return (
     <div className="children-summary">
         {data.groups.map(group => (                 //map functions, interfaces etc separately
-            <div className="group">
-              <div className="subsubtitle">
-                {group.title}
-              </div>
-              <ul className="item-list">
-                {group.children.map(childID => {    //map children within a group
-                  const child = children.find(child => (child.id == childID))
-                  if(exportIds.includes(child.id)) // if child is exported, a link to the child is included.
-                    return (
-                    <li key={child.id + "_child_summary_entry"}>
-                      <SearchLink data={{text: child.name, id: child.id}}/>
-                    </li>)
-                  return (  // if child is not exported, clicking on its name will scroll to its body on the same page
-                    <li key={child.id + "_child_summary_entry_noexport"}>
-                    <div className="sidecontainer" title={child.kindString}>
-                      <button className="clickable-text" onClick={() => scrollTo("#id" + child.id)} onKeyDown={() => scrollTo("#id" + child.id)}>
-                        {child.name}
-                      </button>
-                    </div>
-                  </li>
-                  )
-                })}
-              </ul>
+          <div className="group">
+            <div className="subsubtitle">
+              {group.title}
             </div>
+            <ul className="item-list">
+              {group.children.map(childID => {    //map children within a group,  clicking on its name will scroll to its body on the same page
+                const child = children.find(child => (child.id == childID))
+                return (
+                  <li key={child.id + "_child_summary_entry_noexport"} title={child.kindString}>
+                    <button className="clickable-text" onClick={() => scrollTo("#id" + child.id)} onKeyDown={() => scrollTo("#id" + child.id)}>
+                      {child.name}
+                    </button>
+                </li>
+              )})}
+            </ul>
+          </div>
         ))}
     </div>
   )
--- src/components/general/symbol-title.js
+++ src/components/general/symbol-title.js
@@ -5,23 +5,21 @@ import Type from "../body/type/type"
 export default ({ data, link }) => {
   return (
       <div className="symbol-title inline-block">
-        <div className="sidecontainer">
-          <div className="title">
-              {link ? <Link to={data.fields.path}>{data.name}</Link> : data.name}
-              {data.typeParameter &&
-                  <>
-                      {"<"}
-                      {data.typeParameter.map((tp, i) =>
-                      <>
-                          {i > 0 && <>, </>}
-                          <Type data={{type: tp}}/>
-                      </>)}
-                      {">"}
-                  </>}
-          </div>
-          {data.flags.isOptional &&
-              <div className="bottom optional-mark inline-block" title="isOptional">&nbsp;?&nbsp;</div>}
-      </div>
+          <span className="title">
+            {link ? <Link to={data.fields.path}>{data.name}</Link> : data.name}
+            {data.typeParameter &&
+                <>
+                    {"<"}
+                    {data.typeParameter.map((tp, i) =>
+                    <>
+                        {i > 0 && <>, </>}
+                        <Type data={{type: tp}}/>
+                    </>)}
+                    {">"}
+                </>}
+        </span>
+        {data.flags.isOptional &&
+            <span className="bottom optional-mark inline-block" title="isOptional">&nbsp;?&nbsp;</span>}
     </div>
   )
 }
