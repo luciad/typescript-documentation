59d463be9177592da11706c5c2caab2cf2739faa
tijlb99 <tijl.bormans@student.kuleuven.be>
2020-12-06 17:13:45 +0100

    test fixes

--- cypress/integration/e2e/folder1/class4_spec.js
+++ cypress/integration/e2e/folder1/class4_spec.js
@@ -3,15 +3,15 @@ describe('Class4 test', () => {
         it('Searches Class4 page for expected contents, mostly type and signature-summary', () => {
       cy.visit('http://localhost:8000/modules/folder1/class4/Class4/function')
 
-      cy.get(".signature-summary").contains("function")
-      cy.get(".signature-summary").contains("(")
-      cy.get(".signature-summary").contains("string")
+      cy.contains("function")
+      cy.contains("(")
+      cy.contains("string")
 
-      cy.get(".signature-summary").contains(/^Class0$/).click({force: true})
+      cy.contains(/^Class0$/).click({force: true})
       cy.url().should('include', '/class0/Class0')
 
       cy.visit('http://localhost:8000/modules/folder1/class4/Class4/function')
-      cy.get(".signature-summary").contains(/^Class1$/).click({force: true})
+      cy.contains(/^Class1$/).click({force: true})
       cy.url().should('include', '/class1/Class1')
 
       cy.visit('http://localhost:8000/modules/folder1/class4/Class4/function')
@@ -20,14 +20,14 @@ describe('Class4 test', () => {
       cy.url().should('include', '/class1/Class1')
 
       cy.visit('http://localhost:8000/modules/folder1/class4/Callback')
-      cy.get('.signature-summary').contains("void")
+      cy.contains("void")
 
       cy.visit('http://localhost:8000/modules/folder1/class4/Callback2')
-      cy.get('.signature-summary').contains("string")
-      cy.get('.signature-summary').contains("array")
-      cy.get('.signature-summary').get('a').contains("Class0")
-      cy.get('.signature-summary').get('a').contains("Class1")
-      cy.get('.signature-summary').get('a').contains("MyNumber")
+      cy.contains("string")
+      cy.contains("array")
+      cy.get('a').contains("Class0")
+      cy.get('a').contains("Class1")
+      cy.get('a').contains("MyNumber")
 
       cy.get('.kind-string').contains("Type alias")
     })
--- cypress/integration/e2e/folder1/class6_spec.js
+++ cypress/integration/e2e/folder1/class6_spec.js
@@ -3,13 +3,13 @@ describe('Class6 test', () => {
     it('Searches Class6 page for expected contents, mostly signatures', () => {
   cy.visit('http://localhost:8000/modules/folder1/class6/Class6/constructor')
 
-  cy.get(".signature-summary").contains("new Class6")
-  cy.get(".signature-summary").get('a').contains('FirstClass').click({force: true})
+  cy.contains("new Class6")
+  cy.get('a').contains('FirstClass').click({force: true})
   cy.url().should('include', '/firstclass/FirstClass')
 
   cy.visit('http://localhost:8000/modules/folder1/class6/Class6/property')
-  cy.contains("get")
-  cy.contains("set")
+  cy.contains("read")
+  cy.contains("write")
 
   cy.visit('http://localhost:8000/modules/folder1/class5/myInterface')
   cy.get(".kind-string").contains("Interface")
